476f46f868a5f33f5eef0048838c4b2a360cc3d4
zhuwei <akkako@126.com>
2015-02-16 02:12:24 +0800

    add start.sh for start up proces

--- src/main/assembly/dev/bin/start.sh
+++ src/main/assembly/dev/bin/start.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+base_dir=$(dirname $0)/..
+for file in ${base_dir}/lib/*.jar;
+do
+  CLASSPATH=$CLASSPATH:$file
+done
+
+CLASSPATH=$CLASSPATH:${base_dir}/config
+
+#java -jar Main
